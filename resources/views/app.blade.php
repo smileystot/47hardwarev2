c605da0cd3b7b036370cae2f4f6b9cf01ec73d92
smileystot <robinarias0908@gmail.com>
2024-09-18 10:59:05 +0800

    tailwind-setup

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,11 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>ًApplication</title>
-        @vite('resources/css/app.css')
+        <title>@yield('title')</title>
+        @vite(['resources/css/app.css','resources/js/app.js'])
     </head>
     <body>
         <div id="app"></div>
-        @vite('resources/js/app.js')
     </body>
 </html>
